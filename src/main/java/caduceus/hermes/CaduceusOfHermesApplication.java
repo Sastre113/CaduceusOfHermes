7c15d8047e5bc894b9abd48829274ec3e58f0b3a
Miguel Á. Sastre <sastre113@gmail.com>
2023-07-12 00:29:05 +0200

    initial commit

---
+++ src/main/java/caduceus/hermes/CaduceusOfHermesApplication.java
@@ -0,0 +1,13 @@
+package caduceus.hermes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaduceusOfHermesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaduceusOfHermesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/caduceus/hermes/CaduceusOfHermesApplicationTests.java
@@ -0,0 +1,13 @@
+package caduceus.hermes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaduceusOfHermesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
